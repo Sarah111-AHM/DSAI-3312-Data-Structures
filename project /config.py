1087d9510e13d184d39b3ff62ea77f769be09974
Sarah Abumandil <114596306+Sarah111-AHM@users.noreply.github.com>
2026-03-01 14:12:40 +0200

    Update config.py

--- project /config.py
+++ project /config.py
@@ -1,1 +1,11 @@
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+
+DATA_DIR = BASE_DIR / "data"
+SENTIMENT_LEXICON_PATH = DATA_DIR / "sentiment_lexicon.csv"
+STOPWORDS_PATH = DATA_DIR / "stopwords.txt"
+
+DATA_DIR.mkdir(exist_ok=True)
