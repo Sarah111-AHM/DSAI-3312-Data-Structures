19cac4452d40b971048637f5fa82176a0ce2e6f9
Sarah Abumandil <114596306+Sarah111-AHM@users.noreply.github.com>
2026-03-01 14:25:49 +0200

    Update prediction.py

--- project /features/prediction.py
+++ project /features/prediction.py
@@ -1,1 +1,9 @@
+# features/prediction.py
+from data_structures.ngram import NGramPredictor
 
+class NextWordPredictionFeature:
+    def __init__(self, words):
+        self.predictor = NGramPredictor(words, n=2)  # bigram
+
+    def predict(self, word: str) -> list:
+        return self.predictor.predict([word])
